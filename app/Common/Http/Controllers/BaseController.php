fa4c84c4fe88cc6864e90ff20896f159939ad7f6
safi khan <safifalak@gmail.com>
2020-01-09 12:19:33 +0545

    setting module generated

--- app/Common/Http/Controllers/BaseController.php
+++ app/Common/Http/Controllers/BaseController.php
@@ -6,8 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
-
+use App\Setting\Models\Setting;
 class BaseController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function __construct()
+    {
+        $setting=Setting::all();
+        View::share('setting', $setting);
+    }
 }
